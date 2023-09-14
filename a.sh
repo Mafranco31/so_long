28c39d17e41d6abab979d920277530d5d3b89b14
Mathis <mathis@mbp-de-mathis.home>
2023-09-14 10:36:30 +0200

    oo

--- a.sh
+++
@@ -1,3 +0,0 @@
-gcc src/so_long.c src/checkpath.c src/checkmap.c src/key.c src/init.c src/createmap.c -Wall -Werror -Wextra -L/usr/X11 -lmlx -lXext -lX11 -L./libft -lft -L./ftprintf -lftprintf
-./a.out bermaps/novalidpath.ber
-rm a.out
